e2aba93b8f234e7adbe53a69fa7be45fa6a9a234
ChoiBeomgyuItBoy <octaviortegan@hotmail.com>
2024-01-17 02:27:57 -0600

    Fighter component refactor

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
-using CombatSystem.Core;
+using Cinemachine;
 using RainbowAssets.Utils;
-using System.Collections;
+using CombatSystem.Core;
+using CombatSystem.Attributes;
 
 namespace CombatSystem.Combat
 {
@@ -10,9 +11,14 @@ namespace CombatSystem.Combat
         [SerializeField] WeaponData weaponData;
         [SerializeField] Transform rightHand;
         [SerializeField] Transform leftHand;
+        [SerializeField] float targetRange = 20;
+        [SerializeField] CinemachineTargetGroup targetGroup;
         AnimationPlayer animationPlayer;
+        Health target;
         Weapon weapon;
         int currentAttackIndex = 0;
+        const float targetGroupRadius = 2;
+        const float targetGroupWeight = 1;
 
         void Awake()
         {
@@ -29,14 +35,26 @@ namespace CombatSystem.Combat
             return weaponData.GetAttack(currentAttackIndex);
         }
 
+        float GetCurrentAttackTime()
+        {
+            return animationPlayer.GetAnimationTime("Attack");
+        }
+
         bool CurrentAttackIsLast()
         {
             return currentAttackIndex == weaponData.GetComboLength() - 1;
         }
 
-        float GetCurrentAttackTime()
+        bool CurrentAttackFinished()
         {
-            return animationPlayer.GetAnimationTime("Attack");
+            float successTime = GetCurrentAttack().GetEndTime();
+
+            if(GetCurrentAttackTime() < successTime)
+            {
+                return false;
+            }
+            
+            return true;
         }
 
         void Attack()
@@ -54,29 +72,78 @@ namespace CombatSystem.Combat
             currentAttackIndex = 0;
         }
 
-        bool CanDoCombo()
-        {
-            WeaponAttack currentAttack = GetCurrentAttack();
-
-            if(CurrentAttackIsLast())
-            {
-                return false;
-            }
-
-            float successTime = currentAttack.GetEndTime();
-
-            if(GetCurrentAttackTime() < successTime)
-            {
-                return false;
-            }
-
-            return true;
-        }
-
         // Animation Event
         void Hit()
         {
             weapon.Hit(gameObject, weaponData, GetCurrentAttack());
+        }
+
+        bool SelectTarget()
+        {
+            var hits = Physics.SphereCastAll(transform.position, targetRange, Vector3.up, 0);
+
+            foreach(var hit in hits)
+            {
+                Health target = hit.transform.GetComponent<Health>();
+
+                if(target != null && target != GetComponent<Health>())
+                {
+                    SetTarget(target);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        void SetTarget(Health target)
+        {
+            this.target = target;
+
+            if(targetGroup != null)
+            {
+                targetGroup.AddMember(target.transform, targetGroupWeight, targetGroupRadius);
+            }
+        }
+
+        void CancelTarget()
+        {
+            if(target != null)
+            {
+                if(targetGroup != null)
+                {
+                    targetGroup.RemoveMember(target.transform);
+                }
+
+                target = null;
+            }
+        }
+
+        void FaceTarget()
+        {
+            if(target != null)
+            {
+                Vector3 lookDirection = target.transform.position - transform.position;
+                lookDirection.y = 0;
+                transform.rotation = Quaternion.LookRotation(lookDirection);
+            }
+        }
+
+        bool TargetInRange()
+        {
+            if(target != null)
+            {
+                float targetDistance = (transform.position - target.transform.position).magnitude;
+                return targetDistance <= targetRange;
+            }
+
+            return false;
+        }
+
+        void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, targetRange);
         }
 
         void IAction.DoAction(string actionID, string[] parameters)
@@ -94,6 +161,13 @@ namespace CombatSystem.Combat
                 case "Reset Combo":
                     ResetCombo();
                     break;
+                case "Face Target":
+                    FaceTarget();
+                    break;
+
+                case "Cancel Target":
+                    CancelTarget();
+                    break;
             }
         }
 
@@ -101,10 +175,19 @@ namespace CombatSystem.Combat
         {
             switch(predicate)
             {
-                case "Can Do Combo":
-                    return CanDoCombo();
+                case "Current Attack Finished":
+                    return CurrentAttackFinished();
+
+                case "Current Attack Is Last":
+                    return CurrentAttackIsLast();
+
+                case "Select Target":
+                    return SelectTarget();
+
+                case "Target In Range":
+                    return TargetInRange();
             }
-            
+
             return null;
         }
     }
